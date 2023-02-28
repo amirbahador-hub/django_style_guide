c6f07226321d36c5732d83901f5efe6056037fd8
amirbahador <amirbahador.pv@gmail.com>
2023-02-28 23:27:31 +0200

    Fix authentication import bug

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/authentication/apps.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/authentication/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'devopshobbies.authentication'
+    name = '{{cookiecutter.project_slug}}.authentication'
