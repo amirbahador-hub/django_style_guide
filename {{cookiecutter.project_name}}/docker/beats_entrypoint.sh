095e8a077fc8ce7f015333037b0d3319b2ddd63b
vahid <vahidtwo@gmail.com>
2023-03-09 18:10:30 +0330

    fix celery beat entrypoint config

--- {{cookiecutter.project_name}}/docker/beats_entrypoint.sh
+++ {{cookiecutter.project_name}}/docker/beats_entrypoint.sh
@@ -1,2 +1,2 @@
 echo "--> Starting beats process"
-celery -A {{cookiecutter.project_slug}}.tasks worker -l info --without-gossip --without-mingle --without-heartbeat
+celery -A {{cookiecutter.project_slug}}.tasks beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
