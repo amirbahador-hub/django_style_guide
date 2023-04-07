0ebbbfad29eb19aaca818718cc7566b5f9b1a738
cliali <cliali1181@gmail.com>
2023-04-07 12:20:08 +0330

    fixed coociecutter typo

--- {{cookiecutter.project_name}}/config/asgi.py
+++ {{cookiecutter.project_name}}/config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for embed project.
+ASGI config for {{cookiecutter.project_name}} project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- {{cookiecutter.project_name}}/config/wsgi.py
+++ {{cookiecutter.project_name}}/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for embed project.
+WSGI config for {{cookiecutter.project_name}} project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
