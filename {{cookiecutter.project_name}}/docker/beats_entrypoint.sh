6ab966bc4ed4b4c4cd87be9943f1828e6e19504d
cliali <cliali1181@gmail.com>
2023-04-07 11:43:36 +0330

    fixed docker typos and more

--- {{cookiecutter.project_name}}/docker/beats_entrypoint.sh
+++ {{cookiecutter.project_name}}/docker/beats_entrypoint.sh
@@ -1,2 +1,2 @@
-echo "--> Starting beats process"
-celery -A {{cookiecutter.project_slug}}.tasks worker -l info --without-gossip --without-mingle --without-heartbeat
+echo "--> Starting celery process"
+celery -A {{cookiecutter.project_slug}}.tasks beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
