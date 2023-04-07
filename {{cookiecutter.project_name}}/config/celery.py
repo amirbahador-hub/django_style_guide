8a270eefaaf6c8db5731d8ec2555da01421683ec
cliali <cliali1181@gmail.com>
2023-04-07 12:04:40 +0330

    fixed settings

--- {{cookiecutter.project_name}}/config/celery.py
+++ {{cookiecutter.project_name}}/config/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.django.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.django.local')
 
 celery = Celery('config')
 celery.config_from_object('django.conf:django', namespace='CELERY')
